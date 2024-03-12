def581526682449b8d3596bc027ffac93917e17b
Otavio Portella <otavioportella.ads@gmail.com>
2024-03-12 17:31:30 -0300

    fix-bug-screen-from-login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./components/header";
-import Page from "./components/page";
+import Login from "./components/login";
 import "./index.css";
 
 function App() {
   return (
     <div className="min-h-screen w-screen flex flex-col">
       <Header />
-      <Page />
+      <Login />
     </div>
   );
 }
