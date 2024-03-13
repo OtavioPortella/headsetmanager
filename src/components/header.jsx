d3c8064853b7c7cc47c6194fa1df84a767a96743
Otavio Portella <otavioportella.ads@gmail.com>
2024-03-13 16:53:42 -0300

    excludind-test-button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,9 @@
-import Button from "./button";
-
 function Header() {
   return (
     <header className="bg-azul-toledo text-white h-16">
       <div className="flex justify-between p-4">
         <div className="flex justify-center h-8">
           <img src="../src/assets/toledo.png" alt="" />
-        </div>
-        <div>
-          <Button>
-            <a href="./teste1">Teste1</a>
-          </Button>
-          <Button>
-            <a href="./teste2">Teste2</a>
-          </Button>
         </div>
       </div>
     </header>
