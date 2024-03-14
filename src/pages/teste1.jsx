884c78306ce6e48b05a9021c16ffc4ca6439ac90
Otavio Portella <otavioportella.ads@gmail.com>
2024-03-14 13:15:20 -0300

    changing-received-screen

--- src/pages/teste1.jsx
+++ src/pages/teste1.jsx
@@ -5,26 +5,35 @@ function Teste1() {
     <div className="flex-1 flex justify-center pt-4 pb-4  w-full bg-hero-pattern bg-cover bg-no-repeat">
       <div className="rounded-lg flex flex-col bg-white/35 shadow-2xl">
         <h1 className="font-serif pt-8 text-azul-toledo text-center text-[46px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          delectus rerum, repudiandae distinctio consectetur sint voluptatibus
-          ex reprehenderit blanditiis dolorum a? Quaerat exercitationem repellat
-          placeat facere, repudiandae earum minima accusamus.
+          Heads Recebidos
         </h1>
-        <div className="flex-1 flex justify-between">
-          <div className=" flex-1 flex flex-col justify-center items-center gap-4">
+        <div className=" w-screen">
+          <div className="flex flex-col items-center gap-4">
             <input
               className="bg-blue-200 border border-azul-toledo rounded-md outline-none p-1 placeholder:text-azul-toledo"
-              type="text"
-              placeholder="Usuário"
+              type="date"
+              placeholder=""
             />
             <input
+              className="bg-blue-200 border border-azul-toledo rounded-md outline-none p-1 placeholder:text-azul-toledo"
+              type="number"
+              placeholder="Quantidade"
+            />
+            <label> Selecione a Filial:</label>
+            <select
               className="bg-blue-200 border border-azul-toledo rounded-md outline-none p-1 placeholder:text-azul-toledo"
               type="password"
-              placeholder="Senha"
-            />
+              placeholder="Filial"
+            >
+              <option value="matriz">Matriz</option>
+              <option value="aguapei">Aguapeí</option>
+              <option value="aviacao">Aviação</option>
+              <option value="marechal">Marechal</option>
+              <option value="saverio">Savério</option>
+            </select>
+
             <Button>Entrar</Button>
           </div>
-          <div className="flex-1 w-[700px] bg-forgot-password bg-contain bg-no-repeat" />
         </div>
       </div>
     </div>
