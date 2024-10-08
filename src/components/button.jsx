ae2890bc8405c9742d9af6d120ad8181a334ff81
Otavio Portella <otavioportella.ads@gmail.com>
2024-10-07 23:21:47 -0300

    login com backend

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,8 +1,9 @@
-function Button({ children }) {
+function Button({ children, type, ...rest }) {
   return (
     <button
       className="bg-azul-toledo font-semibold px-4 py-1 text-white rounded-md hover:bg-slate-300 hover:text-azul-toledo hover:border hover:border-azul-toledo"
-      type="button"
+      type={type ?? 'button'}
+      {...rest}
     >
       {children}
     </button>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,45 +1,65 @@
 import Button from "../components/button";
 import Header from "../components/header";
 import Background from "../components/background";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = new FormData(event.target)
+    
+    const matricula = form.get('matricula')
+    const senha = form.get('senha');
+  
+    const response = await axios.post('http://localhost:3000/login', {
+      matricula,
+      senha
+    });
+
+    const { user, token } = response.data;
+
+
+    console.log({
+      user,
+      token
+    });
+
+    navigate('/dashboard')
+    
+  }
+
   return (
     <>
       <Header />
       <Background>
-        <div className="rounded-lg flex flex-col bg-white/35 shadow-2xl">
+        <form className="rounded-lg flex flex-col bg-white/35 shadow-2xl" onSubmit={handleSubmit}>
           <h1 className="font-serif pt-8 text-azul-toledo text-center text-[46px]">
             Login
           </h1>
           <div className="flex-1 flex justify-between">
             <div className="flex-1 flex flex-col items-center justify-center gap-2 p-4">
               <h1>Selecione a sua Filial:</h1>
-              <select
-                className="bg-blue-200 border border-azul-toledo rounded-md outline-none p-1 placeholder:text-azul-toledo"
-                type="password"
-                placeholder="Filial"
-              >
-                <option value="matriz">Matriz</option>
-                <option value="aguapei">Aguapeí</option>
-                <option value="aviacao">Aviação</option>
-                <option value="marechal">Marechal</option>
-                <option value="saverio">Savério</option>
-              </select>
               <input
                 className="bg-blue-200 border border-azul-toledo rounded-md outline-none p-1 placeholder:text-azul-toledo"
                 type="text"
-                placeholder="Usuário"
+                placeholder="Matrícula"
+                name="matricula"
               />
               <input
                 className="bg-blue-200 border border-azul-toledo rounded-md outline-none p-1 placeholder:text-azul-toledo"
                 type="password"
                 placeholder="Senha"
+                name="senha"
               />
-              <Button>Entrar</Button>
+              <Button type="submit">Entrar</Button>
             </div>
             <div className="flex-1 w-[700px] bg-forgot-password bg-contain bg-no-repeat" />
           </div>
-        </div>
+        </form>
       </Background>
     </>
   );
